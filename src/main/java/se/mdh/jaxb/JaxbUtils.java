d2dbc235a7b9eca68bc9f021b35e82fdaca16659
Dennis Lundberg <dennisl@apache.org>
2021-06-11 08:42:22 +0200

    Lägg till en klass med hjälpmetoder som rör JAXB.

---
+++ src/main/java/se/mdh/jaxb/JaxbUtils.java
@@ -0,0 +1,34 @@
+package se.mdh.jaxb;
+
+import java.io.StringWriter;
+import java.io.Writer;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+
+/**
+ * En klass med hjälpmetoder som rör JAXB.
+ *
+ * @author Dennis Lundberg
+ * @since 1.1.0
+ */
+public class JaxbUtils {
+  /**
+   * Hjälpmetod som konverterar ett JAXB-annoterat objekt till en XML-sträng.
+   * http://www.thinkcode.se/blog/2010/01/19/how-to-convert-a-pojo-to-xml-with-jaxb
+   *
+   * @param objekt Det objekt som ska konverteras
+   * @param typer Typer som objektet innehåller
+   * @return En sträng med XML
+   * @throws JAXBException Om det inte går att omvandla objektet till XML
+   */
+  public String toXml(final Object objekt, final Class... typer)
+      throws JAXBException {
+    JAXBContext context = JAXBContext.newInstance(typer);
+    Marshaller marshaller = context.createMarshaller();
+    marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+    marshaller.setProperty(Marshaller.JAXB_FRAGMENT, Boolean.TRUE);
+    Writer writer = new StringWriter();
+    marshaller.marshal(objekt, writer);
+    return writer.toString();
+  }}
